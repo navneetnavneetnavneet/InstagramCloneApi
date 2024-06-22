a4c5597ab456b08c19a85138344e85d47ad9bc0f
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2024-06-22 23:21:44 +0530

    Error Handling Done

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ require("dotenv").config({
 const express = require("express");
 const app = express();
 const logger = require("morgan");
+const ErrorHandler = require("./utils/ErrorHandler");
+const { generatedError } = require("./middlewares/errors");
 
 // logger
 app.use(logger("tiny"));
 
 app.use("/", require("./routes/userRouters"));
+
+// error handleing
+app.all("*", (req, res, next) => {
+  next(new ErrorHandler(`Requested URL Not Found ${req.url}`, 404));
+});
+app.use(generatedError);
 
 // creating server
 app.listen(process.env.PORT, () => {
