2089a98dcc4af5cac9308dd9f014d2e43524bddb
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2024-10-06 16:26:59 +0530

    done

--- models/storyModel.js
+++ models/storyModel.js
@@ -7,22 +7,22 @@ const storySchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
   createdAt: { type: Date, default: Date.now },
-  expiresAt: {
-    type: Date,
-    default: () => Date.now() + 24 * 60 * 60 * 1000,
-    index: { expires: "1d" },
-  },
+  // expiresAt: {
+  //   type: Date,
+  //   default: () => Date.now() + 24 * 60 * 60 * 1000,
+  //   index: { expires: "1d" },
+  // },
 });
 
 // Middleware to remove the story from the user's stories array when it expires
-storySchema.post("remove", async function (story) {
-  try {
-    await mongoose.model("user").findByIdAndUpdate(story.user, {
-      $pull: { stories: story._id },
-    });
-  } catch (error) {
-    console.error("Error updating user stories:", error);
-  }
-});
+// storySchema.post("remove", async function (story) {
+//   try {
+//     await mongoose.model("user").findByIdAndUpdate(story.user, {
+//       $pull: { stories: story._id },
+//     });
+//   } catch (error) {
+//     console.error("Error updating user stories:", error);
+//   }
+// });
 
 module.exports = mongoose.model("story", storySchema);
